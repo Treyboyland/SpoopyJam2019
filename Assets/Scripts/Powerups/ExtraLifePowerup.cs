882c877d04bd959e1becee5ece17845df2b1030f
Roosevelt <treyboyland@aol.com>
2019-10-21 23:34:21 -0500

    Extra life powerup added

---
+++ Assets/Scripts/Powerups/ExtraLifePowerup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraLifePowerup : Powerup, IPowerup
+{
+
+    [SerializeField]
+    int numExtraLives;
+
+    static LifeCounter counter;
+
+    void FindReference()
+    {
+        //Life counter is on the main camera
+        counter = counter == null ? GameObject.FindGameObjectWithTag("MainCamera").GetComponent<LifeCounter>() : counter;
+    }
+
+    public void HandlePowerup(Player p)
+    {
+        FindReference();
+        if (counter != null)
+        {
+            counter.NumLives += numExtraLives;
+        }
+        else
+        {
+            Debug.LogWarning("" + gameObject.name + "NOTE: Unable to find LifeCounter");
+        }
+
+        OnPowerupTaken.Invoke();
+    }
+}
